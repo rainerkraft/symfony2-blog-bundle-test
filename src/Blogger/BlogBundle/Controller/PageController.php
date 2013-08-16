d023fe370db3a78d300fb3eaf31f6d99112a2071
Rainer Kraft <rkraft@gumtree.com>
2013-08-16 13:01:19 +0100

    Initial commit

---
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -0,0 +1,55 @@
+<?php
+// src/Blogger/BlogBundle/Controller/PageController.php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
+
+class PageController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:index.html.twig');
+    }
+
+    public function aboutAction()
+    {
+    	return $this->render('BloggerBlogBundle:Page:about.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+        $enquiry= new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+
+        if ($request->getMethod() === "POST") {
+            
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom($this->container->getParameter('blogger_blog.emails.contact_email_sender'))
+                    ->setTo($this->container->getParameter('blogger_blog.emails.contact_email_recipient'))
+                    ->setBody($this->renderView('BloggerBlogBundle:Email:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->getFlashBag()->add(
+                    'blogger-notice',
+                    'Your contact enquiry was successfully sent. Thank you!'
+                );
+
+                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+            }
+        }
+
+    	return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+}
