81b0f53fe055c346dd5747ec027deb5fa64fe06a
Rainer Kraft <rkraft@gumtree.com>
2013-08-16 14:59:18 +0100

    Adding blog/comment controller, repository and fixtures

---
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,32 @@
+<?php
+// src/Blogger/BlogBundle/Controller/BlogController
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Blog controller
+ */
+class BlogController extends Controller
+{
+	/**
+	 * [showAction description]
+	 * @param  int $id ID of the blog entry
+	 * @return view
+	 */
+	public function showAction($id)
+	{
+		$em = $this->getDoctrine()->getEntityManager();
+
+		$blog = $em->getRepository('BloggerBlogBundle:Blog')->find($id);
+
+		if (!$blog) {
+			throw $this->createNotFoundException('Unable to find blog post.');
+		}
+
+		return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+			'blog' => $blog,
+		));
+	}
+}
