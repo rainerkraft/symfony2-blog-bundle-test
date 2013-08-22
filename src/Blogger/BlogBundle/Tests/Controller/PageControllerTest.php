fd81506e12cc665ee91e454318c81511b479f42e
Rainer Kraft <rkraft@gumtree.com>
2013-08-22 17:16:21 +0100

    Added Contact test

--- src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
+++ src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
@@ -46,4 +46,28 @@ class PageControllerTest extends WebTestCase
         // Check that the H2 has the blog title in it
         $this->assertEquals(1, $crawler->filter('article.blog h2:contains("' . $blogTitle . '")')->count());
     }
+
+    public function testContact()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $this->baseUrl . '/contact');
+
+        $this->assertEquals(1, $crawler->filter('h1:contains("Contact symblog")')->count());
+
+        // Select based on button value, or id or name for buttons
+        $form = $crawler->selectButton('Submit')->form();
+
+        $form['contact[name]'] = 'name';
+        $form['contact[email]'] = 'email@email.com';
+        $form['contact[subject]'] = 'Subject';
+        $form['contact[body]'] = 'The comment body must be at least 50 characters long as there is a validation constraint on the Enquiry entity';
+
+        $crawler = $client->submit($form);
+
+        // Need to follow redirect
+        $crawler = $client->followRedirect();
+
+        $this->assertEquals(1, $crawler->filter('.blogger-notice:contains("Your contact enquiry was successfully sent. Thank you!")')->count());
+    }
 }
