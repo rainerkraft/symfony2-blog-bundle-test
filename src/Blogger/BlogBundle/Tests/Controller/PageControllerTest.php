64e2fe05a6e8b5ba932e4619da405ccf26cac05f
Rainer Kraft <jonuxuk@gmail.com>
2013-08-23 10:50:43 +0100

    Added email test for PageController contact

--- src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
+++ src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
@@ -66,6 +66,22 @@ class PageControllerTest extends WebTestCase
 
         $client->submit($form);
 
+        // Check email has been sent
+        if ($profile = $client->getProfile()) {
+            $swiftMailerProfiler = $profile->getCollector('swiftmailer');
+
+            // Only 1 message should have been sent
+            $this->assertEquals(1, $swiftMailerProfiler->getMessageCount());
+
+            // Get the first message
+            $messages = $swiftMailerProfiler->getMessages();
+            $message = array_shift($messages);
+
+            $symblogEmail = $client->getContainer()->getParameter('blogger_blog.emails.contact_email');
+            // Check message is being sent to correct address
+            $this->assertArrayHasKey($symblogEmail, $message->getTo());
+        }
+
         // Need to follow redirect
         $crawler = $client->followRedirect();
 
